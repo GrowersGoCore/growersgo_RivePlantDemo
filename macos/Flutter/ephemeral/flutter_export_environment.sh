9125a347ffc4f9c67a16bc8aa4436d3986cadebc
RaulHidalgoRodriguez2000 <166127083+RaulHidalgoRodriguez2000@users.noreply.github.com>
2024-08-20 16:38:01 +0200

    par15

--- macos/Flutter/ephemeral/flutter_export_environment.sh
+++ macos/Flutter/ephemeral/flutter_export_environment.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
 export "FLUTTER_ROOT=/Users/raul/Documents/sdk/flutter"
-export "FLUTTER_APPLICATION_PATH=/Users/raul/Documents/demo_tomato"
+export "FLUTTER_APPLICATION_PATH=/Users/raul/Documents/GitHub/growersgo_RivePlantDemo"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
 export "FLUTTER_BUILD_DIR=build"
 export "FLUTTER_BUILD_NAME=1.0.0"
